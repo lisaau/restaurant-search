3f25d43a75f6986f6700a4f15bafba536cfdeccf
lisaau <lisaau@users.noreply.github.com>
2020-04-10 14:45:08 -0700

    Add search screen and update react navigation

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
-import HomeScreen from "./src/screens/HomeScreen";
+import SearchScreen from "./src/screens/SearchScreen";
 
 const navigator = createStackNavigator(
   {
-    Home: HomeScreen
+    Search: SearchScreen
   },
   {
-    initialRouteName: "Home",
+    initialRouteName: "Search",
     defaultNavigationOptions: {
-      title: "App"
+      title: "Business Search"
     }
   }
 );
