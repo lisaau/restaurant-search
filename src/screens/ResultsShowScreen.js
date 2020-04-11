d51de7c743ca6492fc8c19170ac4774f80fae1ab
lisaau <lisaau@users.noreply.github.com>
2020-04-11 15:44:16 -0700

    Set up hook to fetch info on single restaurant

--- src/screens/ResultsShowScreen.js
+++ src/screens/ResultsShowScreen.js
@@ -1,9 +1,22 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { Text, StyleSheet, View } from "react-native";
+import yelp from '../api/yelp';
 
 const ResultsShowScreen= ({ navigation }) => {
+    const [result, setResult] = useState(null);
+
+    console.log(result);
+    
     // how we will get information from the second argument that we called nagivate with
     const id = navigation.getParam('id');
+
+    const getResult = async (id) => {
+        const response = await yelp.get(`/${id}`);
+        setResult(response.data);
+    }
+    useEffect(() => {
+        getResult(id)
+    }, [])
 
     return (
         <View style={styles.container}>
