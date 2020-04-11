f3df48547695757060e3fcecb931f6e5b34a8555
lisaau <lisaau@users.noreply.github.com>
2020-04-10 19:18:13 -0700

    Group restaurant results from API call

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -8,6 +8,13 @@ const SearchScreen = () => {
   const [term, setTerm] = useState('');
   const [searchAPI, restaurants, errorMessage] = useRestaurants();
 
+  const filterRestaurantsByPrice = (price) => {
+    // price === '$' || '$$' || '$$$' || '$$$$'
+    return restaurants.filter(result => {
+      return result.price === price;
+    });
+  };
+
   return <View>
     <SearchBar 
       term={term} 
@@ -16,9 +23,10 @@ const SearchScreen = () => {
     />
     {errorMessage ? <Text style={styles.text}>{errorMessage}</Text> : null}
     <Text>We have found {restaurants.length} restaurants</Text>
-    <ResultsList title='Cost Effective' />
-    <ResultsList title='Bit Pricier' />
-    <ResultsList title='Big Spender' />
+    <ResultsList results={filterRestaurantsByPrice('$')} title='Cost Effective' />
+    <ResultsList results={filterRestaurantsByPrice('$$')} title='Bit Pricier' />
+    <ResultsList results={filterRestaurantsByPrice('$$$')} title='Big Spender' />
+    <ResultsList results={filterRestaurantsByPrice('$$$$')} title='Really Big Spender' />
   </View>
 };
 
